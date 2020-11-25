ece06479ba728111f536ca1738716b57a40c6354
James Sapp <jameseisapp@gmail.com>
2020-11-25 21:33:56 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,16 +16,7 @@ end
 
 def consolidate_cart(cart)
   new_cart = [] 
-  cart.each do |item| 
-    item_name = item.keys[0]
-    if new_cart[item_name]
-      new_cart[item_name][:count] += 1 
-    else
-      new_cart[item_name] = item[item_name]
-      new_cart[item_name][:count] = 1 
-    end
-  end
-  new_cart
+
     
 end
 
